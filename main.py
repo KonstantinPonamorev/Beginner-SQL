23e41825b562e0362f6890e026d10346144e66ed
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2022-02-14 22:13:22 +0300

    SQL Create Correct

--- main.py
+++ main.py
@@ -6,27 +6,47 @@ connection = engine.connect()
 
 # connection.execute("""DELETE FROM artist""")
 
-# connection.execute("""
-#     INSERT INTO artist
-#     VALUES(1, 'Bill Murrey');
-#     INSERT INTO artist
-#     VALUES(2, 'Joe Crossby');
-#     INSERT INTO artist
-#     VALUES(3, 'Monica Belucci');
-#     INSERT INTO artist
-#     VALUES(4, 'Eddie Murphy');
-#     INSERT INTO artist
-#     VALUES(5, 'Oleg Bulygin');
-#     INSERT INTO artist
-#     VALUES(6, 'Constantine Ponamorev');
-#     INSERT INTO artist
-#     VALUES(7, 'Mega Artist');
-#     INSERT INTO artist
-#     VALUES(8, 'Whoare You');
-#     """)
+connection.execute("""
+    INSERT INTO artist
+    VALUES(1, 'Bill Murrey');
+    INSERT INTO artist
+    VALUES(2, 'Joe Crossby');
+    INSERT INTO artist
+    VALUES(3, 'Monica Belucci');
+    INSERT INTO artist
+    VALUES(4, 'Eddie Murphy');
+    INSERT INTO artist
+    VALUES(5, 'Oleg Bulygin');
+    INSERT INTO artist
+    VALUES(6, 'Constantine Ponamorev');
+    INSERT INTO artist
+    VALUES(7, 'Mega Artist');
+    INSERT INTO artist
+    VALUES(8, 'Whoare You');
+    """)
 
-sel = connection.execute("""
-    SELECT * FROM artist;
-    """).fetchall()
+connection.execute("""
+    INSERT INTO genre
+    VALUES(1, 'Bill Murrey');
+    INSERT INTO artist
+    VALUES(2, 'Joe Crossby');
+    INSERT INTO artist
+    VALUES(3, 'Monica Belucci');
+    INSERT INTO artist
+    VALUES(4, 'Eddie Murphy');
+    INSERT INTO artist
+    VALUES(5, 'Oleg Bulygin');
+    INSERT INTO artist
+    VALUES(6, 'Constantine Ponamorev');
+    INSERT INTO artist
+    VALUES(7, 'Mega Artist');
+    INSERT INTO artist
+    VALUES(8, 'Whoare You');
+    """)
 
-pprint(sel)
+
+# sel = connection.execute("""
+#     SELECT * FROM artist;
+#     """).fetchall()
+#
+# pprint(sel)
